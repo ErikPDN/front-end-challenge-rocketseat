6067d2028ab38eb186383a5d6e672908c0928759
Erik-Arch-Notebook <erikpdn1@gmail.com>
2025-04-10 00:05:52 -0300

    feat: creating header

---
+++ capputeeno/src/app/components/header.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { styled } from "styled-components";
+import { Saira_Stencil_One } from "next/font/google";
+import { PrimaryInputWSearchIcon } from "./primary-input";
+import { CartControl } from "./cart-control";
+
+interface HeaderProps {
+
+}
+
+const sairaStencil = Saira_Stencil_One({
+  subsets: ["latin"],
+  weight: ["400"],
+});
+
+const TagHeader = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px 160px;
+
+  > div {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  
+    gap: 24px;
+  }
+`
+const Logo = styled.a`
+  color: var(--logo-color);
+  font-size: 40px;
+  font-weight: 400;
+  line-height: 150%;
+`
+
+export function Header(props: HeaderProps) {
+  return (
+    <TagHeader>
+      <Logo className={sairaStencil.className}>Capputeeno</Logo>
+      <div>
+        <PrimaryInputWSearchIcon placeholder="Procurando por algo específico" />
+        <CartControl />
+      </div>
+    </TagHeader>
+  )
+}
